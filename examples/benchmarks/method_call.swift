d32fc9c089d20b6d82fd9da23742e18c497bfe41
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-06 17:52:35 -0300

    Added method_call benchmark. Worked on the File class a little more, stuck on the variadic open call.

---
+++ examples/benchmarks/method_call.swift
@@ -0,0 +1,89 @@
+
+// This is a Swift version of a custom benchmark first introduced on the Wren
+// project here:
+// https://github.com/munificent/wren/blob/master/test/benchmark/method_call.dart
+
+
+class Toggle {
+
+  var state = false
+
+  init(startState: Bool) {
+    state = startState
+  }
+
+  var value: Bool {
+    get { return state }
+  }
+
+  func activate() -> Toggle {
+    state = !state
+    return self
+  }
+
+}
+
+
+class NthToggle: Toggle {
+
+  var count = 0
+  var countMax = 0
+
+  init(startState: Bool, maxCounter: Int) {
+    super.init(startState: startState)
+    countMax = maxCounter
+    count = 0
+  }
+
+  override func activate() -> Toggle {
+    count = count + 1
+    if (count >= countMax) {
+      super.activate()
+      count = 0
+    }
+
+    return self
+  }
+}
+
+
+//  Stopwatch watch = new Stopwatch();
+//  watch.start();
+
+let n = 100000
+var val = true
+let toggle = Toggle(startState: val)
+
+for i in 0..<n {
+  val = toggle.activate().value
+  val = toggle.activate().value
+  val = toggle.activate().value
+  val = toggle.activate().value
+  val = toggle.activate().value
+  val = toggle.activate().value
+  val = toggle.activate().value
+  val = toggle.activate().value
+  val = toggle.activate().value
+  val = toggle.activate().value
+}
+
+print(toggle.value)
+
+val = true
+let ntoggle = NthToggle(startState: val, maxCounter: 3)
+
+for i in 0..<n {
+  val = ntoggle.activate().value
+  val = ntoggle.activate().value
+  val = ntoggle.activate().value
+  val = ntoggle.activate().value
+  val = ntoggle.activate().value
+  val = ntoggle.activate().value
+  val = ntoggle.activate().value
+  val = ntoggle.activate().value
+  val = ntoggle.activate().value
+  val = ntoggle.activate().value
+}
+
+print(ntoggle.value)
+//print("elapsed: ${watch.elapsedMilliseconds / 1000}");
