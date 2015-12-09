570c6d6fa1bac51716b37c324c2ddee68305bce7
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-09 01:29:16 -0300

    Learned about Swift's debugPrint method and used it for Sua's lang.

--- Sources/lang.swift
+++ Sources/lang.swift
@@ -1,50 +1,16 @@
 
 public func escapeString(s: String) -> String {
   var z = ""
-  for c in s.characters {
-    if c == "\n" {
-      z += "\\n"
-    } else if c == "\"" {
-      z += "\\\""
-    } else if c == "\\" {
-      z += "\\\\"
-    } else {
-      z.append(c)
-    }
-  }
-  return "\"\(z)\""
+  debugPrint(s, terminator: "", toStream: &z)
+  return z
 }
 
-public func inspect(o: String) -> String {
-  return escapeString(o)
+public func inspect(o: Any) -> String {
+  var z = ""
+  debugPrint(o, terminator: "", toStream: &z)
+  return z
 }
 
-public func inspect(o: Int) -> String {
-  return String(o)
-}
-
-public func inspect(o: CustomStringConvertible) -> String {
-  return o.description
-}
-
-public func inspect(o: Any?) -> String {
-  return String(o)
-}
-
-public func p(a: String...) {
-  for e in a {
-    print(inspect(e))
-  }
-}
-
-public func p(a: Any?...) {
-  for e in a {
-    print(e)
-  }
-}
-
-public func p(a: CustomStringConvertible...) {
-  for e in a {
-    print(e)
-  }
+public func p(a: Any...) {
+  debugPrint(a)
 }
