113840e715602cd3a7f65ef9b86cef9cb2396d59
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-08 21:40:10 -0300

    Added some core lang methods to start the Sua package with.

---
+++ Sources/lang.swift
@@ -0,0 +1,52 @@
+
+func escapeString(s: String) -> String {
+  var z = ""
+  for c in s.characters {
+    if c == "\n" {
+      z += "\\n"
+    } else if c == "\"" {
+      z += "\\\""
+    } else if c == "\\" {
+      z += "\\\\"
+    } else {
+      z.append(c)
+    }
+  }
+  return "\"\(z)\""
+}
+
+func inspect(o: String) -> String {
+  return escapeString(o)
+}
+
+func inspect(o: Int) -> String {
+  return String(o)
+}
+
+func inspect(o: CustomStringConvertible) -> String {
+  print("ever")
+  return o.description
+}
+
+func inspect(o: Any?) -> String {
+  print("ever more")
+  return String(o)
+}
+
+func p(a: String...) {
+  for e in a {
+    print(inspect(e))
+  }
+}
+
+func p(a: Any?...) {
+  for e in a {
+    print(e)
+  }
+}
+
+func p(a: CustomStringConvertible...) {
+  for e in a {
+    print(e)
+  }
+}
