d9100717e2d71003fbf1a8829bd8f83aca750687
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-10 19:49:04 -0300

    Added description to the String extension.

--- Sources/string.swift
+++ Sources/string.swift
@@ -2,6 +2,16 @@
 
 public extension String {
 
+    /**
+        fromCharCodes can take an array of char codes from which a new
+        String will be generated.
+        The start and end parameters can be omitted.
+        The end parameter is inclusive. If the end parameter is negative,
+        the end will be interpreted to be the value of the count of the array.
+        **Note**: if the range includes a null value (0), since the String
+        will be generated using a null-delimited C String function, the
+        String may be truncated at the null value instead.
+    */
     static func fromCharCodes(charCodes: [CChar], start: Int = 0,
         end: Int = -1) -> String {
       let len = charCodes.count
