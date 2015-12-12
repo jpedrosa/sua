0371ebb73b7354c1c46aafbd0a99ca82e61a2793
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-12 15:36:29 -0300

    Small tweak to method documentation.

--- Sources/string.swift
+++ Sources/string.swift
@@ -2,43 +2,43 @@
 
 public extension String {
 
-    /**
-      fromCharCodes takes an array of char codes from which a new String will
-      be generated.
+  /**
+    It takes an array of char codes from which a new String will
+    be generated.
 
-      The start and end parameters can be omitted.
+    The start and end parameters can be omitted.
 
-      The end parameter is inclusive. If the end parameter is negative,
-      the end will be interpreted to be the value of the count of the array.
+    The end parameter is inclusive. If the end parameter is negative, the end
+    will be interpreted to be the value of the count of the array minus 1.
 
-      **Note**: if the range includes a null value (0), since the String
-      will be generated using a null-delimited C String function, the
-      String may be truncated at the null value instead.
+    **Note**: if the range includes a null value (0), since the String
+    will be generated using a null-delimited C String function, the
+    String may be truncated at the null value instead.
 
-      E.g.:
-          var a: [CChar] = [72, 101, 108, 108, 111]
-          print(String.fromCharCodes(a)) // Prints Hello
-          print(String.fromCharCodes(a, start: 1, end: 3)) // Prints ell
-    */
-    public static func fromCharCodes(charCodes: [CChar], start: Int = 0,
-        end: Int = -1) -> String {
-      let len = charCodes.count
-      let lasti = len - 1
-      let ei = end < 0 ? lasti : end
-      if (ei < start) {
-        return String()
+    E.g.:
+        var a: [CChar] = [72, 101, 108, 108, 111]
+        print(String.fromCharCodes(a)) // Prints Hello
+        print(String.fromCharCodes(a, start: 1, end: 3)) // Prints ell
+  */
+  public static func fromCharCodes(charCodes: [CChar], start: Int = 0,
+      end: Int = -1) -> String {
+    let len = charCodes.count
+    let lasti = len - 1
+    let ei = end < 0 ? lasti : end
+    if (ei < start) {
+      return String()
+    } else {
+      if ei < lasti {
+        let zi = ei + 1
+        var a = [CChar](charCodes[start...zi])
+        a[a.count - 1] = 0
+        return String.fromCString(&a)!
       } else {
-        if ei < lasti {
-          let zi = ei + 1
-          var a = [CChar](charCodes[start...zi])
-          a[a.count - 1] = 0
-          return String.fromCString(&a)!
-        } else {
-          var a = [CChar](charCodes[start...lasti])
-          a.append(0)
-          return String.fromCString(&a)!
-        }
+        var a = [CChar](charCodes[start...lasti])
+        a.append(0)
+        return String.fromCString(&a)!
       }
     }
+  }
 
 }
