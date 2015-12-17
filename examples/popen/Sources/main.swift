e5ecf0916a274003621b38d1b5a2bc2e9736a76e
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-17 02:01:35 -0300

    Started the Popen example.

---
+++ examples/popen/Sources/main.swift
@@ -0,0 +1,35 @@
+
+import Glibc
+
+
+try PopenStream.readLines("ls -l /home/dewd/t_") { s in
+  Stdout.write(s ?? "")
+}
+
+try PopenStream.readLines("lssdsdsd -l /home/dewd/t_") { s in
+  Stdout.write(s ?? "")
+}
+
+try PopenStream.readLines("find /home/dewd/t_ -type f") { s in
+  Stdout.write(s ?? "")
+}
+
+try IO.popen("cd /home/dewd/t_ && grep -ir a.*b") { s in
+  Stdout.write(s ?? "")
+}
+
+try PopenStream.readBytes("ls -l /home/dewd/t_") { a, len in
+  Stdout.writeBytes(a, length: len)
+}
+
+try PopenStream.readBytes("lssdsdsd -l /home/dewd/t_") { a, len in
+  Stdout.writeBytes(a, length: len)
+}
+
+try PopenStream.readBytes("find /home/dewd/t_ -type f") { a, len in
+  Stdout.writeBytes(a, length: len)
+}
+
+try IO.popenBytes("cd /home/dewd/t_ && grep -ir a.*b") { a, len in
+  Stdout.writeBytes(a, length: len)
+}
