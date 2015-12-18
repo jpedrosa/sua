2e349f2d0d3cda3c29d0666c0ae6a3e385319f54
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-18 03:12:23 -0300

    Added support for MurmurHash 128 bits and fixed the switch statement of the 32 bits version.

--- Sources/murmurhash3.swift
+++ Sources/murmurhash3.swift
@@ -37,7 +37,9 @@ public class MurmurHash3 {
     // remainder
     switch maxBytes & 3 { // `len % 4'
       case 3: k ^= UInt32(tail[2] << 16)
+          fallthrough
       case 2: k ^= UInt32(tail[1] << 8)
+          fallthrough
       case 1:
         k ^= UInt32(tail[0])
         k = k &* c1
@@ -73,6 +75,151 @@ public class MurmurHash3 {
       seed: UInt32 = 0) -> UInt32 {
     var a = key
     return doHash32(&a, maxBytes: maxBytes, seed: seed)
+  }
+
+  // MurmurHash3 128 bits.
+  // Translated it to Swift using the following references:
+  // * [qhash.c](https://github.com/wolkykim/qlibc/blob/8e5e6669fae0eb63e4fb171e7c84985b0828c720/src/utilities/qhash.c)
+  // * [MurmurHash3.cpp](https://code.google.com/p/smhasher/source/browse/trunk/MurmurHash3.cpp)
+
+  public static func doHash128(key: UnsafePointer<UInt8>, maxBytes: Int,
+      seed: UInt64 = 0) -> (h1: UInt64, h2: UInt64) {
+    let c1: UInt64 = 0x87c37b91114253d5
+    let c2: UInt64 = 0x4cf5ad432745937f
+    let nblocks = maxBytes / 16
+    var h1 = seed
+    var h2 = seed
+    var k1: UInt64 = 0
+    var k2: UInt64 = 0
+
+    let blocks = UnsafeBufferPointer<UInt64>(
+          start: UnsafePointer<UInt64>(key), count: nblocks)
+    let tail = UnsafeBufferPointer<UInt8>(
+          start: UnsafePointer<UInt8>(key) + (nblocks * 16), count: 16)
+
+    for i in 0..<nblocks {
+      k1 = blocks[(i * 2) + 0]
+      k2 = blocks[(i * 2) + 1]
+
+      k1 = k1 &* c1
+      k1 = (k1 << 31) | (k1 >> (64 - 31))
+      k1 = k1 &* c2
+      h1 ^= k1
+
+      h1 = (h1 << 27) | (h1 >> (64 - 27))
+      h1 = h1 &+ h2
+      h1 = (h1 &* 5) &+ 0x52dce729
+
+      k2 = k2 &* c2
+      k2 = (k2 << 33) | (k2 >> (64 - 33))
+      k2 = k2 &* c1
+      h2 ^= k2
+
+      h2 = (h2 << 31) | (h2 >> (64 - 31))
+      h2 = h2 &+ h1
+      h2 = (h2 &* 5) &+ 0x38495ab5
+    }
+
+    k1 = 0
+    k2 = 0
+
+    // Remainder.
+    switch maxBytes & 15 { // maxBytes % 15
+      case 15:
+          k2 ^= UInt64(tail[14]) << 48
+          fallthrough
+      case 14:
+          k2 ^= UInt64(tail[13]) << 40
+          fallthrough
+      case 13:
+          k2 ^= UInt64(tail[12]) << 32
+          fallthrough
+      case 12:
+          k2 ^= UInt64(tail[11]) << 24
+          fallthrough
+      case 11:
+          k2 ^= UInt64(tail[10]) << 16
+          fallthrough
+      case 10:
+          k2 ^= UInt64(tail[9]) << 8
+          fallthrough
+      case 9:
+          k2 ^= UInt64(tail[8]) << 0
+          k2 = k2 &* c2
+          k2 = (k2 << 33) | (k2 >> (64 - 33))
+          k2 = k2 &* c1
+          h2 ^= k2
+          fallthrough
+      case 8:
+          k1 ^= UInt64(tail[7]) << 56
+          fallthrough
+      case 7:
+          k1 ^= UInt64(tail[6]) << 48
+          fallthrough
+      case 6:
+          k1 ^= UInt64(tail[5]) << 40
+          fallthrough
+      case 5:
+          k1 ^= UInt64(tail[4]) << 32
+          fallthrough
+      case 4:
+          k1 ^= UInt64(tail[3]) << 24
+          fallthrough
+      case 3:
+          k1 ^= UInt64(tail[2]) << 16
+          fallthrough
+      case 2:
+          k1 ^= UInt64(tail[1]) << 8
+          fallthrough
+      case 1:
+          k1 ^= UInt64(tail[0]) << 0
+          k1 = k1 &* c1
+          k1 = (k1 << 31) | (k1 >> (64 - 31))
+          k1 = k1 &* c2
+          h1 ^= k1
+      default: () // Ignore.
+    }
+
+    h1 ^= UInt64(maxBytes)
+    h2 ^= UInt64(maxBytes)
+
+    h1 = h1 &+ h2
+    h2 = h2 &+ h1
+
+    h1 ^= h1 >> 33
+    h1 = h1 &* 0xff51afd7ed558ccd
+    h1 ^= h1 >> 33
+    h1 = h1 &* 0xc4ceb9fe1a85ec53
+    h1 ^= h1 >> 33
+
+    h2 ^= h2 >> 33
+    h2 = h2 &* 0xff51afd7ed558ccd
+    h2 ^= h2 >> 33
+    h2 = h2 &* 0xc4ceb9fe1a85ec53
+    h2 ^= h2 >> 33
+
+    h1 = h1 &+ h2
+    h2 = h2 &+ h1
+
+    return (h1, h2)
+  }
+
+  public static func hash128(key: String, seed: UInt64 = 0)
+      -> (h1: UInt64, h2: UInt64) {
+    var a = [UInt8](key.utf8)
+    return doHash128(&a, maxBytes: a.count, seed: seed)
+  }
+
+  public static func hash128CChar(key: [CChar], maxBytes: Int,
+      seed: UInt64 = 0) -> (h1: UInt64, h2: UInt64) {
+    let ap = UnsafePointer<UInt8>(key)
+    return doHash128(ap, maxBytes: maxBytes, seed: seed)
+  }
+
+  public static func hash128Bytes(key: [UInt8], maxBytes: Int,
+      seed: UInt64 = 0) -> (h1: UInt64, h2: UInt64) {
+    var a = key
+    return doHash128(&a, maxBytes: maxBytes, seed: seed)
   }
 
 }
--- examples/murmur/Sources/main.swift
+++ examples/murmur/Sources/main.swift
@@ -19,3 +19,15 @@ var c: [UInt8] = [112, 97, 110, 100, 97]
 p(MurmurHash3.hash32Bytes(c, maxBytes: c.count, seed: 10))
 
 p(MurmurHash3.hash32("panda", seed: 10))
+
+p("-------")
+
+p(MurmurHash3.hash128("kinkajou"))
+
+p(MurmurHash3.hash128CChar(a, maxBytes: a.count))
+
+p(MurmurHash3.hash128CChar(b, maxBytes: b.count, seed: 10))
+
+p(MurmurHash3.hash128Bytes(c, maxBytes: c.count, seed: 10))
+
+p(MurmurHash3.hash128("panda", seed: 10))
