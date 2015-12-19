6edf8e45a448436950db64ec4e27a5ff2bcdb481
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-18 23:54:46 -0300

    Added the stopwatch_it example.

---
+++ examples/stopwatch_it/Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+  name:  "StopwatchIt",
+  dependencies: [
+    .Package(url: "../../", majorVersion: 0)
+  ]
+)
---
+++ examples/stopwatch_it/Sources/main.swift
@@ -0,0 +1,64 @@
+
+import Glibc
+import Sua
+import CSua
+
+var sw = Stopwatch()
+
+var rng = RNG()
+let NI = 30
+
+sw.start()
+for _ in 0..<NI {
+  p(rng.nextInt(10))
+}
+p("It took \(sw.millis)ms to generate these \(NI) random ints.")
+
+let ND = 30
+
+sw.start()
+for _ in 0..<ND {
+  p(rng.nextDouble())
+}
+p("It took \(sw.millis)ms to generate these \(ND) random doubles.")
+
+var m = [String: String]()
+
+sw.start()
+var filesCount = 0
+FileBrowser.recurseDir("/home/dewd/t_") { (name, type, path) in
+  if type == .F {
+    filesCount += 1
+    var fp = "\(path)\(name)"
+    try! File.open(fp) { f in
+      do {
+        var a = try f.readBytes()
+        m[fp] = String(MurmurHash3.hash32Bytes(a, maxBytes: a.count))
+      } catch {
+        p("(failed to read file: \(fp))")
+      }
+    }
+  }
+}
+p("It took \(sw.millis)ms to hash the \(filesCount) files in these directories.")
+
+var i = 0
+for (k, v) in m {
+  p("\(k): \(v)")
+  if i > 10 {
+    break
+  }
+  i += 1
+}
+
+print("Starting stopwatch again...")
+
+sw.start()
+sleep(1)
+sw.stop()
+print("Elapsed: \(sw.millis)")
+sw.start()
+sleep(2)
+print("Elapsed: \(sw.millis)")
+sleep(3)
+print("Elapsed: \(sw.millis)")
