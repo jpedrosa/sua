f275e7a237ec0b061834b063ce3d9fd7ba350634
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-22 00:44:48 -0300

    Added actual word counting support to the word counter example.

--- examples/word_counter/Sources/main.swift
+++ examples/word_counter/Sources/main.swift
@@ -7,6 +7,7 @@ if !Stdin.isTerminal {
   var stream = CodeUnitStream()
   var bytesCount = 0
   var newLineCount = 0
+  var wordCount = 0
 
   try Stdin.readByteLines() { line in
     let len = line.count
@@ -14,12 +15,22 @@ if !Stdin.isTerminal {
     if len > 0 && line[len - 1] == 10 {
       newLineCount += 1
     }
-    p(line)
-    p(String.fromCharCodes(line))
-    //stream.codeUnits = line
+    //p(line)
+    //p(String.fromCharCodes(line))
+    stream.codeUnits = line
+    while !stream.isEol {
+      // 32 - space; 10 - new line; 9 - tab; 13 - carriage return
+      // 160 - unicode space: \u00a0
+      stream.eatWhile { (c: UInt8) in
+        return c == 32 || c == 10 || c == 9 || c == 13 || c == 160
+      }
+      if stream.eatWhileNeitherFive(32, c2: 10, c3: 9, c4: 13, c5: 160) {
+        wordCount += 1
+      }
+    }
   }
 
-  print("  \(newLineCount)  ( - )   \(bytesCount)")
+  print("  \(newLineCount)   \(wordCount)   \(bytesCount)")
 } else {
   print("Usage: call it by passing a pipe or standard input.\n" +
     "E.g. WordCounter < some_sample.txt\n" +
