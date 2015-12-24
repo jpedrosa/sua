e177a9313b87421905b747067652dde36e572b5a
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-23 21:40:20 -0300

    Added another version of the SplitOnce algorithm to the benchmark. It was created by someone else over on Reddit.

--- examples/split_once/Sources/main.swift
+++ examples/split_once/Sources/main.swift
@@ -21,12 +21,41 @@ extension String.CharacterView {
 
 }
 
+extension String.UTF16View {
+
+  private func splitOnceFB2(at: String.UTF16View)
+    -> (String.UTF16View, String.UTF16View)? {
+      guard let c = at.first else { return nil }
+      let second = at.startIndex.successor()
+      for i in startIndex..<endIndex.advancedBy(1-at.count) where self[i] == c {
+        var (j,k) = (second,i.successor())
+        repeat {
+          if j == at.endIndex {
+            return (prefixUpTo(i), suffixFrom(k))
+          } else if k == endIndex {
+            break
+          }
+        } while self[k++] == at[j++]
+      }
+      return nil
+  }
+
+}
+
+
 extension String {
 
   func splitOnceFB(at: String) -> (String, String)? {
     return self
       .characters
       .splitOnceFB(at.characters)
+      .map { (a,b) in (String(a), String(b)) }
+  }
+
+  public func splitOnceFB2(string: String) -> (String, String)? {
+    return self
+      .utf16
+      .splitOnceFB2(string.utf16)
       .map { (a,b) in (String(a), String(b)) }
   }
 
@@ -37,7 +66,7 @@ let sample = "abcdefghijklmnopqrstuvxywz"
 let sw = Stopwatch()
 var count = 0
 
-p("splitOnceFB#splitOnceFB")
+p("String#splitOnceFB")
 sw.start()
 for _ in 0..<ITERATIONS {
   let (key, value) = sample.splitOnceFB("kl")!
@@ -64,6 +93,17 @@ sw.start()
 for _ in 0..<ITERATIONS {
   let (key, value) = sample.splitOnce("kl")
   if !key!.isEmpty {
+    count += 1
+  }
+}
+p("Elapsed: \(sw.millis)ms count: \(count)")
+
+count = 0
+p("String#splitOnceFB2")
+sw.start()
+for _ in 0..<ITERATIONS {
+  let (key, value) = sample.splitOnceFB2("kl")!
+  if !key.isEmpty {
     count += 1
   }
 }
