726b4567435cf3697642d19384f85a68d539322f
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-25 02:01:53 -0300

    Added a track dir benchmark example.

---
+++ examples/benchmarks/track_dir/Sources/main.swift
@@ -0,0 +1,79 @@
+
+import Glibc
+import Sua
+
+
+var count = 0
+
+func trackDir(pathBytes: [UInt8]) {
+  let cap = UnsafePointer<CChar>(pathBytes)
+  let dp = opendir(cap)
+  if dp == nil {
+    // Ignore in case permission denies access to the directory.
+    return
+  }
+  defer {
+    closedir(dp)
+  }
+  var entry = readdir(dp)
+  while entry != nil {
+    count += 1
+    if entry.memory.d_type == 4 {
+      var dirName = entry.memory.d_name
+      withUnsafePointer(&dirName) { ptr in
+        let len = Int(Sys.strlen(UnsafePointer<CChar>(ptr)))
+        let b = UnsafeBufferPointer<UInt8>(start: UnsafePointer<UInt8>(ptr),
+            count: len)
+        if len <= 2 && b[0] == 46 && (len == 1 || b[1] == 46) {
+          // Ignore Dot file: . ..
+        } else {
+          var cp = pathBytes
+          cp.removeLast() // Remove null-byte 0
+          cp += [UInt8](b)
+          cp.append(47)
+          cp.append(0) // Now append the trailing null-byte 0
+          trackDir(cp)
+        }
+      }
+    }
+    entry = readdir(dp)
+  }
+}
+
+func spelunkDir(path: String) {
+  let dp = opendir(path)
+  if dp == nil {
+    // Ignore in case permission denies access to the directory.
+    return
+  }
+  defer {
+    closedir(dp)
+  }
+  var entry = readdir(dp)
+  while entry != nil {
+    count += 1
+    if entry.memory.d_type == 4 {
+      var dirName = entry.memory.d_name
+      withUnsafePointer(&dirName) { ptr in
+        if let name = String.fromCString(UnsafePointer<CChar>(ptr)) {
+          if name != "." && name != ".." {
+            spelunkDir("\(path)\(name)/")
+          }
+        }
+      }
+    }
+    entry = readdir(dp)
+  }
+}
+
+var sw = Stopwatch()
+var a = [UInt8]("/".utf8)
+a.append(0)
+sw.start()
+trackDir(a)
+p("count: \(count) Elapsed: \(sw.millis)")
+
+count = 0
+sw.start()
+spelunkDir("/")
+p("count: \(count) Elapsed: \(sw.millis)")
