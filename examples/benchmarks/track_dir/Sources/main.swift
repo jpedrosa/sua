96d6545519c7efc89004f6d1271802506fe8b1fe
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-26 05:21:39 -0300

    Added peekDir function to the track dir benchmark example. It uses an algorithm without recursion.

--- examples/benchmarks/track_dir/Sources/main.swift
+++ examples/benchmarks/track_dir/Sources/main.swift
@@ -119,6 +119,65 @@ func browseDir2(pathBytes: [UInt8]) throws {
   }
 }
 
+func peekDir(pathBytes: [UInt8]) {
+  var pb = pathBytes
+  var store = [([UInt8], COpaquePointer)]()
+  store.reserveCapacity(128)
+  PATH: while store.count >= 0 { // Trick the compiler.
+    let cap = UnsafePointer<CChar>(pb)
+    var dp = opendir(cap)
+    if dp == nil {
+      // Ignore in case permission denies access to the directory.
+      if store.count > 0 {
+        (pb, dp) = store.removeLast()
+      } else {
+        break PATH
+      }
+    }
+    DIRP: while store.count >= 0 { // Trick the compiler.
+      var entry = readdir(dp)
+      while entry != nil {
+        count += 1
+        if entry.memory.d_type == 4 {
+          var dirName = entry.memory.d_name
+          var ignore = true
+          withUnsafePointer(&dirName) { ptr in
+            let len = Int(Sys.strlen(UnsafePointer<CChar>(ptr)))
+            let b = UnsafeBufferPointer<UInt8>(start: UnsafePointer<UInt8>(ptr),
+                count: len)
+            if len <= 2 && b[0] == 46 && (len == 1 || b[1] == 46) {
+              // Ignore Dot file: . ..
+            } else {
+              var cp = [UInt8]()
+              cp.reserveCapacity(pb.count + len + 1)
+              cp += pb
+              cp.removeLast() // Remove null-byte 0
+              cp += b
+              cp.append(47)
+              cp.append(0) // Now append the trailing null-byte 0
+              store.append((pb, dp))
+              pb = cp
+              ignore = false
+            }
+          }
+          if !ignore {
+            continue PATH
+          }
+        }
+        entry = readdir(dp)
+      }
+      closedir(dp)
+      if store.count > 0 {
+        (pb, dp) = store.removeLast()
+        continue DIRP
+      } else {
+        break PATH
+      }
+    }
+  }
+}
+
+
 
 var sw = Stopwatch()
 var a = [UInt8]("/".utf8)
@@ -149,6 +208,11 @@ do {
   // ignore errors.
 }
 p("browseDir2 count: \(count) Elapsed: \(sw.millis)")
+
+count = 0
+sw.start()
+peekDir(a)
+p("peekDir    count: \(count) Elapsed: \(sw.millis)")
 
 count = 0
 sw.start()
