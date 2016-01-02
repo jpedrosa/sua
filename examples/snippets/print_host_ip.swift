8caabd72ab20b7170a21d3508539df9bcb44d11a
Joao Pedrosa <joaopedrosa@gmail.com>
2016-01-02 14:16:19 -0300

    Added print host ip snippet example.

---
+++ examples/snippets/print_host_ip.swift
@@ -0,0 +1,41 @@
+
+import Glibc
+
+
+func printHostIP(hostName: String) {
+  var hints = addrinfo()
+  hints.ai_family = AF_INET
+  hints.ai_socktype = Int32(SOCK_STREAM.rawValue)
+  hints.ai_flags = AI_ADDRCONFIG
+  hints.ai_protocol = Int32(IPPROTO_TCP)
+  var info = UnsafeMutablePointer<addrinfo>()
+  let status = getaddrinfo(hostName, nil, &hints, &info)
+  if status == 0 {
+    var h = info.memory
+    while true {
+      let fam = h.ai_family
+      let len = fam == AF_INET ? INET_ADDRSTRLEN : INET6_ADDRSTRLEN
+      var descBuffer = [CChar](count: Int(len), repeatedValue: 0)
+      var sockaddr = h.ai_addr.memory
+      withUnsafePointer(&sockaddr) { ptr in
+        let sin = UnsafePointer<sockaddr_in>(ptr)
+        var sin_addr = sin.memory.sin_addr
+        if inet_ntop(fam, &sin_addr, &descBuffer, UInt32(len)) != nil {
+          let z = String.fromCString(descBuffer) ?? ""
+          print("Host: \(hostName), IP: \(z)")
+        }
+      }
+      let next = h.ai_next
+      if next == nil {
+        break
+      } else {
+        h = next.memory
+      }
+    }
+  } else {
+    print("Error: couldn't find address for \"\(hostName)\").")
+  }
+}
+
+printHostIP("google.com")
+printHostIP("reddit.com")
