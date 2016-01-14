af51cfb86ef9c033bc207b9bccfb2bf3765566c5
Joao Pedrosa <joaopedrosa@gmail.com>
2016-01-14 18:50:49 -0300

    Updated the Popen example to match the latest APIs.

--- examples/popen/Sources/main.swift
+++ examples/popen/Sources/main.swift
@@ -4,34 +4,34 @@ import Sua
 import CSua
 
 
-try PopenStream.readLines("ls -l /home/dewd/t_") { s in
+try Popen.readLines("ls -l /home/dewd/t_") { s in
   Stdout.write(s ?? "")
 }
 
-try PopenStream.readLines("lssdsdsd -l /home/dewd/t_") { s in
+try Popen.readLines("lssdsdsd -l /home/dewd/t_") { s in
   Stdout.write(s ?? "")
 }
 
-try PopenStream.readLines("find /home/dewd/t_ -type f") { s in
+try Popen.readLines("find /home/dewd/t_ -type f") { s in
   Stdout.write(s ?? "")
 }
 
-try IO.popen("cd /home/dewd/t_ && grep -ir a.*b") { s in
+try Popen.readLines("cd /home/dewd/t_ && grep -ir a.*b") { s in
   Stdout.write(s ?? "")
 }
 
-try PopenStream.readBytes("ls -l /home/dewd/t_") { a, len in
+try Popen.readByteLines("ls -l /home/dewd/t_") { a, len in
   Stdout.writeBytes(a, maxBytes: len)
 }
 
-try PopenStream.readBytes("lssdsdsd -l /home/dewd/t_") { a, len in
+try Popen.readByteLines("lssdsdsd -l /home/dewd/t_") { a, len in
   Stdout.writeBytes(a, maxBytes: len)
 }
 
-try PopenStream.readBytes("find /home/dewd/t_ -type f") { a, len in
+try Popen.readByteLines("find /home/dewd/t_ -type f") { a, len in
   Stdout.writeBytes(a, maxBytes: len)
 }
 
-try IO.popenBytes("cd /home/dewd/t_ && grep -ir a.*b") { a, len in
+try Popen.readByteLines("cd /home/dewd/t_ && grep -ir a.*b") { a, len in
   Stdout.writeBytes(a, maxBytes: len)
 }
