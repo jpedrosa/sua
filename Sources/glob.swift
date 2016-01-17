d4715855e79bae0766dd0505f05f1431de2258d6
Joao Pedrosa <joaopedrosa@gmail.com>
2016-01-17 01:39:39 -0300

    Added Lexer and GlobLexer classes.

---
+++ Sources/glob.swift
@@ -0,0 +1,232 @@
+
+public enum GlobLexerTokenizer: LexerTokenizer {
+  case OptionalNameComma
+  case OptionalName
+  case OptionalNameMaybeEmpty
+  case SetMinusChar
+  case SetLowerCaseMinus
+  case SetUpperCaseMinus
+  case SetDigitMinus
+  case Set
+  case SetCharMaybeEmpty
+  case SetNegation
+  case SetMaybeEmpty
+  case Body
+  case Root
+  case Text
+}
+
+
+enum GlobTokenType: TokenType {
+  case Text
+  case Separator
+  case Name
+  case Symbol
+}
+
+
+class GlobLexer: CommonLexer {
+
+  typealias T = GlobLexerTokenizer
+
+  override init(stream: ByteStream) {
+    let st = CommonLexerStatus(tokenizer: T.Root, spaceTokenizer: nil)
+    super.init(stream: stream, status: st)
+    defaultTokenizer = T.Text
+  }
+
+  override func next(tokenizer: LexerTokenizer) -> TokenType {
+    switch(tokenizer as! GlobLexerTokenizer) {
+      case .OptionalNameComma:
+        return inOptionalNameComma()
+      case .OptionalName:
+        return inOptionalName()
+      case .OptionalNameMaybeEmpty:
+        return inOptionalNameMaybeEmpty()
+      case .SetMinusChar:
+        return inSetMinusChar()
+      case .SetLowerCaseMinus:
+        return inSetLowerCaseMinus()
+      case .SetUpperCaseMinus:
+        return inSetUpperCaseMinus()
+      case .SetDigitMinus:
+        return inSetDigitMinus()
+      case .Set:
+        return inSet()
+      case .SetCharMaybeEmpty:
+        return inSetCharMaybeEmpty()
+      case .SetNegation:
+        return inSetNegation()
+      case .SetMaybeEmpty:
+        return inSetMaybeEmpty()
+      case .Body:
+        return inBody()
+      case .Root:
+        return inRoot()
+      case .Text:
+        return inText()
+    }
+  }
+
+  static func keywordString(type: GlobTokenType) -> String {
+    switch type {
+      case .Text:
+        return "text"
+      case .Separator:
+        return "Separator"
+      case .Name:
+        return "Name"
+      case .Symbol:
+        return "Symbol"
+    }
+  }
+
+  func isContextChar(c: UInt8) -> Bool {
+    return c == 47 || c == 42 || c == 63 || c == 123 || c == 91 // / * ? { [
+  }
+
+  func inOptionalNameComma() -> GlobTokenType {
+    if stream.eatComma() {
+      status.tokenizer = T.OptionalName
+      return .Symbol
+    } else if stream.eatCloseBrace() {
+      status.tokenizer = T.Body
+      return .Symbol
+    }
+    return inText()
+  }
+
+  func inOptionalName() -> GlobTokenType {
+    if stream.eatWhileNeitherTwo(125, c2: 44) { // } ,
+      status.tokenizer = T.OptionalNameComma
+      return .Name
+    }
+    return inText()
+  }
+
+  func inOptionalNameMaybeEmpty() -> GlobTokenType {
+    if stream.eatCloseBrace() {
+      status.tokenizer = T.Body
+      return .Symbol
+    }
+    return inOptionalName()
+  }
+
+  func inSetMinusChar() -> GlobTokenType {
+    stream.next()
+    status.tokenizer = T.Set
+    return .Name
+  }
+
+  func inSetLowerCaseMinus() -> GlobTokenType {
+    stream.eatMinus()
+    if stream.matchLowerCase() >= 0 {
+      status.tokenizer = T.SetMinusChar
+      return .Symbol
+    }
+    status.tokenizer = T.Set
+    return .Name
+  }
+
+  func inSetUpperCaseMinus() -> GlobTokenType {
+    stream.eatMinus()
+    if stream.matchUpperCase() >= 0 {
+      status.tokenizer = T.SetMinusChar
+      return .Symbol
+    }
+    status.tokenizer = T.Set
+    return .Name
+  }
+
+  func inSetDigitMinus() -> GlobTokenType {
+    stream.eatMinus()
+    if stream.matchDigit() >= 0 {
+      status.tokenizer = T.SetMinusChar
+      return .Symbol
+    }
+    status.tokenizer = T.Set
+    return .Name
+  }
+
+  func inSet() -> GlobTokenType {
+    status.tokenizer = T.Set
+    if stream.eatCloseBracket() {
+      status.tokenizer = T.Body
+      return .Symbol
+    } else if stream.eatLowerCase() {
+      if stream.matchMinus() {
+        status.tokenizer = T.SetLowerCaseMinus
+      }
+      return .Name
+    } else if stream.eatUpperCase() {
+      if stream.matchMinus() {
+        status.tokenizer = T.SetUpperCaseMinus
+      }
+      return .Name
+    } else if stream.eatDigit() {
+      if stream.matchMinus() {
+        status.tokenizer = T.SetDigitMinus
+      }
+      return .Name
+    } else if stream.next() != nil {
+      return .Name
+    }
+    return inText()
+  }
+
+  func inSetCharMaybeEmpty() -> GlobTokenType {
+    if stream.eatCloseBracket() {
+      return inText()
+    }
+    return inSet()
+  }
+
+  func inSetNegation() -> GlobTokenType {
+    if stream.eatCircumflex() {
+      status.tokenizer = T.Set
+      return .Symbol
+    }
+    return inSetCharMaybeEmpty()
+  }
+
+  func inSetMaybeEmpty() -> GlobTokenType {
+    if stream.eatCloseBracket() {
+      return inText()
+    }
+    return inSetNegation()
+  }
+
+  func inBody() -> GlobTokenType {
+    if stream.eatSlash() {
+      return .Separator
+    } else if stream.eatAsterisk() {
+      stream.eatAsterisk()
+      return .Symbol
+    } else if stream.eatQuestionMark() {
+      return .Symbol
+    } else if stream.eatOpenBrace() {
+      status.tokenizer = T.OptionalNameMaybeEmpty
+      return .Symbol
+    } else if stream.eatOpenBracket() {
+      status.tokenizer = T.SetMaybeEmpty
+      return .Symbol
+    }
+    stream.eatUntil(isContextChar)
+    return .Name
+  }
+
+  func inRoot() -> GlobTokenType {
+    status.tokenizer = T.Body
+    if stream.eatSlash() {
+      return .Separator
+    }
+    return inBody()
+  }
+
+  func inText() -> GlobTokenType {
+    stream.skipToEnd()
+    status.tokenizer = T.Text
+    return .Text
+  }
+
+}
---
+++ Sources/lexer.swift
@@ -0,0 +1,216 @@
+
+
+public protocol LexerByteStream {
+  var bytes: [UInt8] { get }
+  var startIndex: Int { get set }
+  var currentIndex: Int { get set }
+  var isEol: Bool { get }
+  var currentTokenString: String? { get }
+  var lineEndIndex: Int { get set }
+  func findIndex(c: UInt8, startAt: Int) -> Int
+}
+
+
+extension ByteStream: LexerByteStream { }
+
+
+public protocol LexerStatus {
+  var tokenizer: LexerTokenizer? { get set }
+  var spaceTokenizer: LexerTokenizer? { get set }
+}
+
+
+public protocol LexerTokenizer { }
+
+
+public protocol TokenType { }
+
+
+protocol Lexer {
+
+  var stream: LexerByteStream { get }
+  var status: LexerStatus { get }
+  var lineCount: Int { get set }
+  var lineStartIndex: Int { get set }
+  var newLineKeyword: Int { get }
+
+  init(stream: LexerByteStream)
+
+  func next(entry: LexerTokenizer) -> TokenType?
+
+  func parse(fn: (type: TokenType?) -> Void)
+
+  func parseTokenStrings(fn: (type: TokenType?, string: String?) -> Void)
+
+}
+
+
+public struct CommonLexerStatus: LexerStatus {
+
+  public var tokenizer: LexerTokenizer?
+  public var spaceTokenizer: LexerTokenizer?
+
+}
+
+
+public struct StoreLexerStatus: LexerStatus {
+
+  public var tokenizer: LexerTokenizer?
+  public var spaceTokenizer: LexerTokenizer?
+  var stored: [LexerTokenizer] = []
+  var indent = 0
+
+  init(tokenizer: LexerTokenizer? = nil,
+      spaceTokenizer: LexerTokenizer? = nil) {
+    self.tokenizer = tokenizer
+    self.spaceTokenizer = spaceTokenizer
+  }
+
+  func clone() -> StoreLexerStatus {
+    var o = StoreLexerStatus(tokenizer: tokenizer,
+        spaceTokenizer: spaceTokenizer)
+    o.indent = indent
+    for t in stored {
+      o.stored.append(t)
+    }
+    return o
+  }
+
+  mutating func push(t: LexerTokenizer) {
+    stored.append(t)
+  }
+
+  mutating func pop() -> LexerTokenizer { return stored.removeLast() }
+
+  mutating func unshift(e: LexerTokenizer) {
+    stored.insert(e, atIndex: 0)
+  }
+
+  mutating func shift() -> LexerTokenizer {
+    return stored.removeAtIndex(0)
+  }
+
+}
+
+
+func ==(lhs: StoreLexerStatus, rhs: StoreLexerStatus) -> Bool {
+  return "\(lhs.tokenizer)" == "\(rhs.tokenizer)" &&
+      lhs.indent == rhs.indent &&
+      "\(lhs.spaceTokenizer)" == "\(rhs.spaceTokenizer)" &&
+      (lhs.stored.count == rhs.stored.count &&
+        "a\(lhs.stored)" == "\(rhs.stored)")
+}
+
+
+public class CommonLexer: CustomStringConvertible {
+
+  var defaultTokenizer: LexerTokenizer?
+  var entryTokenizer: LexerTokenizer?
+  var spaceTokenizer: LexerTokenizer?
+  var lineCount = 0
+  var newLineKeyword: TokenType?
+  var lineStartIndex = 0
+  var stream: ByteStream
+  var status: CommonLexerStatus
+
+  init(stream: ByteStream) {
+    self.stream = stream
+    self.status = CommonLexerStatus()
+  }
+
+  init(stream: ByteStream, status: CommonLexerStatus) {
+    self.stream = stream
+    self.status = status
+  }
+
+  // Override me.
+  func next(tokenizer: LexerTokenizer) -> TokenType? {
+    return nil
+  }
+
+  func parseLine(fn: (type: TokenType?) -> Void) {
+    lineCount += 1
+    while !stream.isEol {
+      var tt: TokenType?
+      if let st = status.spaceTokenizer {
+        tt = next(st)
+      }
+      if tt == nil {
+        if let t = status.tokenizer {
+          tt = next(t)
+          if tt == nil {
+            if let dt = defaultTokenizer {
+              tt = next(dt)
+            }
+          }
+        }
+      }
+      //p([stream.currentTokenString, status.tokenizer]);
+      //p(status.stored);
+      fn(type: tt)
+      stream.startIndex = stream.currentIndex
+    }
+    //p(status.indent);
+  }
+
+  func parseTokenStrings(fn: (type: TokenType?, string: String?) -> Void) {
+    parse() { tt in
+      return fn(type: tt, string: self.stream.currentTokenString)
+    }
+  }
+
+  func parse(fn: (type: TokenType?) -> Void) {
+    let len = stream.bytes.count
+    var ninc = 0
+    var hasNewLine = false
+    let newLineKey = newLineKeyword
+    let hasNewLineKey = newLineKey != nil
+    var si = 0
+    repeat {
+      var i = stream.findIndex(UInt8(10), startAt: si)
+      hasNewLine = i >= 0
+      if !hasNewLine {
+        i = len
+      } else if i > 0 && stream.bytes[i - 1] == 13 {
+        // CR. Handle crlf newline.
+        i -= 1
+        ninc = 2
+      } else {
+        ninc = 1
+      }
+      if i - si > 0 {
+        stream.lineEndIndex = i
+        parseLine(fn)
+        if stream.currentIndex >= len {
+          break
+        }
+        if hasNewLineKey && hasNewLine {
+          stream.startIndex = i
+          stream.currentIndex = i + ninc
+          fn(type: newLineKey)
+        }
+      } else {
+        if hasNewLineKey && hasNewLine {
+          stream.startIndex = i
+          stream.currentIndex = i + ninc
+          fn(type: newLineKey)
+        }
+        lineCount += 1
+      }
+      si = i + ninc
+      stream.startIndex = si
+      stream.currentIndex = si
+      lineStartIndex = si
+      } while si < len
+  }
+
+  func spawnStatus() -> LexerStatus {
+    return CommonLexerStatus(tokenizer: self.entryTokenizer,
+        spaceTokenizer: self.spaceTokenizer)
+  }
+
+  public var description: String {
+    return "CommonLexer(lineCount: \(lineCount))"
+  }
+
+}
