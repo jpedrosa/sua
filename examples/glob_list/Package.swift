126602c5933bc6f1911c7e327c907bba70d83317
Joao Pedrosa <joaopedrosa@gmail.com>
2016-01-22 13:43:32 -0300

    Added the glob_list example.

---
+++ examples/glob_list/Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+  name:  "GlobList",
+  dependencies: [
+    .Package(url: "../../", majorVersion: 0)
+  ]
+)
---
+++ examples/glob_list/Sources/main.swift
@@ -0,0 +1,41 @@
+
+import Glibc
+import Sua
+
+
+func printUsage() {
+  print("GlobList: GlobList <pattern>\n" +
+      "Where the pattern follows a Glob pattern, including a directory path\n" +
+      "that may include a recursion command.\n" +
+      "E.g. GlobList ~/**/*.txt\n\n" +
+      "Table of supported Glob features:\n" +
+      "    * The ? wildcard - It will match a single character of any kind.\n" +
+      "\n" +
+      "    * The * wildcard - It will match any character until the next\n" +
+      "                       pattern is found.\n" +
+      "    * The [a-z] [abc] [A-Za-z0-9_] character set - It will match a\n" +
+      "                       character included in its ranges or sets.\n" +
+      "    * The [!a-z] [!def] character set negation. It will match a\n" +
+      "                       character that is not included in the set.\n" +
+      "    * The {jpg,png} optional names - It will match one of the names\n" +
+      "                       included in its list.\n\n" +
+      "The special characters could be escaped with the \ backslash\n" +
+      "character in order to allow them to work like any other character.")
+}
+
+func parseOpt() -> String? {
+  return Process.arguments.count > 1 Process.arguments[1] : nil
+}
+
+
+if let s = parseOpt() {
+  var z = s
+  if s.utf16.codeUnitAt(0) == 126 { // ~
+    z = File.expandPath(s)
+  }
+  Dir.glob(z) { name, type, path in
+    print("\(type): \(path)\(name)")
+  }
+} else {
+  printUsage()
+}
