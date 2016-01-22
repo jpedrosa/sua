5b0ef7dfff51ab8b44f2fe2a7a7972183330c50e
Joao Pedrosa <joaopedrosa@gmail.com>
2016-01-21 23:05:25 -0300

    Added FileGlobList subscript support to Dir.

--- Sources/dir.swift
+++ Sources/dir.swift
@@ -1,9 +1,28 @@
 
 
-public class Dir {
+public struct DirImpl {
 
-  public static var cwd: String? {
+  public var cwd: String? {
     return Sys.cwd
   }
 
+  public subscript(pattern: String) -> [(String, FileType, String)] {
+    var r = [(String, FileType, String)]()
+    do {
+      var list = try FileGlobList(pattern: pattern) { name, type, path in
+        r.append((name, type, path))
+      }
+      try list.list()
+    } catch {
+      // Ignore it, since subscript cannot throw. Users can use FileGlobList
+      // directly if they want to try to catch the errors.
+      // These errors could also be caused by trying to open directories that
+      // are inaccessible.
+    }
+    return r
+  }
+
 }
+
+
+let Dir = DirImpl()
