c6b9ef55f3fcf9934600b767bddacbc0139f8f50
Joao Pedrosa <joaopedrosa@gmail.com>
2016-01-22 11:04:27 -0300

    Added a couple of handy glob methods to Dir.

--- Sources/dir.swift
+++ Sources/dir.swift
@@ -7,20 +7,32 @@ public struct DirImpl {
   }
 
   public subscript(pattern: String) -> [(String, FileType, String)] {
-    var r = [(String, FileType, String)]()
     do {
-      var list = try FileGlobList(pattern: pattern) { name, type, path in
-        r.append((name, type, path))
-      }
-      try list.list()
+      return try globList(pattern)
     } catch {
       // Ignore it, since subscript cannot throw. Users can use FileGlobList
       // directly if they want to try to catch the errors.
       // These errors could also be caused by trying to open directories that
       // are inaccessible.
-      return [] // Return it empty to be on the safe side.
     }
+    return []
+  }
+
+  public func globList(pattern: String, skipDotFiles: Bool = true,
+      ignoreCase: Bool = false) throws -> [(String, FileType, String)] {
+    var r = [(String, FileType, String)]()
+    var list = try FileGlobList(pattern: pattern) { name, type, path in
+      r.append((name, type, path))
+    }
+    try list.list()
     return r
+  }
+
+  public func glob(pattern: String, skipDotFiles: Bool = true,
+      ignoreCase: Bool = false, fn: FileBrowserHandler) throws {
+    var list = try FileGlobList(pattern: pattern, skipDotFiles: skipDotFiles,
+        ignoreCase: ignoreCase, fn: fn)
+    try list.list()
   }
 
 }
