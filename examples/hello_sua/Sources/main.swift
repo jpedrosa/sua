03cad761fec06a1a43bf85c396d80851654df823
Joao Pedrosa <joaopedrosa@gmail.com>
2016-08-02 23:20:24 -0300

    Updated the HelloSua example to Swift 3.

--- examples/hello_sua/Sources/main.swift
+++ examples/hello_sua/Sources/main.swift
@@ -2,7 +2,8 @@
 import Glibc
 import Sua
 
-p("Hello, Sua!")
+
+p(a: "Hello, Sua!")
 
 let a = "|/-\\|/-\\".characters
 
@@ -11,15 +12,15 @@ func label(c: Character) {
   s.append(c)
   s += " Welcome! "
   s.append(c)
-  Stdout.write(s)
+  Stdout.write(s: s)
 }
 
 for _ in 0..<10 {
   for c in a {
-    label(c)
-    IO.sleep(0.08)
+    label(c: c)
+    IO.sleep(f: 0.08)
   }
 }
 
-label(a.first!)
+label(c: a.first!)
 print("")
