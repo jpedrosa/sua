7ecd16f332025ebf13fc1acced33c9752dcf59fb
Joao Pedrosa <joaopedrosa@gmail.com>
2016-08-04 00:52:03 -0300

    Updated the TrackDir example to Swift 3.

--- examples/benchmarks/track_dir/Sources/main.swift
+++ examples/benchmarks/track_dir/Sources/main.swift
@@ -8,8 +8,7 @@ var count = 0
 
 func trackDir(pathBytes: [UInt8]) {
   let cap = UnsafePointer<CChar>(pathBytes)
-  let dp = opendir(cap)
-  if dp == nil {
+  guard let dp = opendir(cap) else {
     // Ignore in case permission denies access to the directory.
     return
   }
@@ -19,10 +18,10 @@ func trackDir(pathBytes: [UInt8]) {
   var entry = readdir(dp)
   while entry != nil {
     count += 1
-    if entry.memory.d_type == 4 {
-      var dirName = entry.memory.d_name
+    if entry!.pointee.d_type == 4 {
+      var dirName = entry!.pointee.d_name
       withUnsafePointer(&dirName) { ptr in
-        let len = Int(Sys.strlen(UnsafePointer<CChar>(ptr)))
+        let len = Int(Sys.strlen(sp: UnsafePointer<CChar>(ptr)))
         let b = UnsafeBufferPointer<UInt8>(start: UnsafePointer<UInt8>(ptr),
             count: len)
         if len <= 2 && b[0] == 46 && (len == 1 || b[1] == 46) {
@@ -35,7 +34,7 @@ func trackDir(pathBytes: [UInt8]) {
           cp += b
           cp.append(47)
           cp.append(0) // Now append the trailing null-byte 0
-          trackDir(cp)
+          trackDir(pathBytes: cp)
         }
       }
     }
@@ -44,8 +43,7 @@ func trackDir(pathBytes: [UInt8]) {
 }
 
 func spelunkDir(path: String) {
-  let dp = opendir(path)
-  if dp == nil {
+  guard let dp = opendir(path) else {
     // Ignore in case permission denies access to the directory.
     return
   }
@@ -55,13 +53,12 @@ func spelunkDir(path: String) {
   var entry = readdir(dp)
   while entry != nil {
     count += 1
-    if entry.memory.d_type == 4 {
-      var dirName = entry.memory.d_name
+    if entry!.pointee.d_type == 4 {
+      var dirName = entry!.pointee.d_name
       withUnsafePointer(&dirName) { ptr in
-        if let name = String.fromCString(UnsafePointer<CChar>(ptr)) {
-          if name != "." && name != ".." {
-            spelunkDir("\(path)\(name)/")
-          }
+        let name = String(cString: UnsafePointer<CChar>(ptr))
+        if name != "." && name != ".." {
+          spelunkDir(path: "\(path)\(name)/")
         }
       }
     }
@@ -70,58 +67,66 @@ func spelunkDir(path: String) {
 }
 
 func browseDir(pathBytes: [UInt8]) throws {
-  let fb = try FileBrowser(pathBytes: pathBytes)
-  while fb.next() {
-    count += 1
-    if fb.type == .D {
-      let nameBytes = fb.nameBytes
-      let len = nameBytes.count
-      if len <= 2 && nameBytes[0] == 46 && (len == 1 || nameBytes[1] == 46) {
-        // Ignore Dot file: . ..
-      } else {
-        var cp = [UInt8]()
-        cp.reserveCapacity(pathBytes.count + nameBytes.count + 1)
-        cp += pathBytes
-        cp.removeLast() // Remove null-byte 0
-        cp += nameBytes
-        cp.append(47)
-        cp.append(0) // Now append the trailing null-byte 0
-        try browseDir(cp)
+  do {
+    let fb = try FileBrowser(pathBytes: pathBytes)
+    while fb.next() {
+      count += 1
+      if fb.type == .D {
+        let nameBytes = fb.nameBytes
+        let len = nameBytes.count
+        if len <= 2 && nameBytes[0] == 46 && (len == 1 || nameBytes[1] == 46) {
+          // Ignore Dot file: . ..
+        } else {
+          var cp = [UInt8]()
+          cp.reserveCapacity(pathBytes.count + nameBytes.count + 1)
+          cp += pathBytes
+          cp.removeLast() // Remove null-byte 0
+          cp += nameBytes
+          cp.append(47)
+          cp.append(0) // Now append the trailing null-byte 0
+          try browseDir(pathBytes: cp)
+        }
       }
     }
+  } catch {
+    // Ignore in case of errors or denied permission.
   }
 }
 
 func browseDir2(pathBytes: [UInt8]) throws {
-  let fb = try FileBrowser(pathBytes: pathBytes)
-  while fb.next() {
-    count += 1
-    if fb.type == .D {
-      var rn = fb.rawEntry.memory.d_name
-      try withUnsafePointer(&rn) { ptr in
-        let len = Int(Sys.strlen(UnsafePointer<CChar>(ptr)))
-        let b = UnsafeBufferPointer<UInt8>(start: UnsafePointer<UInt8>(ptr),
-            count: len)
-        if len <= 2 && b[0] == 46 && (len == 1 || b[1] == 46) {
-          // Ignore Dot file: . ..
-        } else {
-          var cp = [UInt8]()
-          cp.reserveCapacity(pathBytes.count + len + 1)
-          cp += pathBytes
-          cp.removeLast() // Remove null-byte 0
-          cp += b
-          cp.append(47)
-          cp.append(0) // Now append the trailing null-byte 0
-          try browseDir2(cp)
+  do {
+    let fb = try FileBrowser(pathBytes: pathBytes)
+    while fb.next() {
+      count += 1
+      if fb.type == .D {
+        var rn = fb.rawEntry.pointee.d_name
+        try withUnsafePointer(&rn) { ptr in
+          let len = Int(Sys.strlen(sp: UnsafePointer<CChar>(ptr)))
+          let b = UnsafeBufferPointer<UInt8>(start: UnsafePointer<UInt8>(ptr),
+              count: len)
+          if len <= 2 && b[0] == 46 && (len == 1 || b[1] == 46) {
+            // Ignore Dot file: . ..
+          } else {
+            var cp = [UInt8]()
+            cp.reserveCapacity(pathBytes.count + len + 1)
+            cp += pathBytes
+            cp.removeLast() // Remove null-byte 0
+            cp += b
+            cp.append(47)
+            cp.append(0) // Now append the trailing null-byte 0
+            try browseDir2(pathBytes: cp)
+          }
         }
       }
     }
+  } catch {
+    // Ignore in case of errors or denied permission.
   }
 }
 
 func peekDir(pathBytes: [UInt8]) {
   var pb = pathBytes
-  var store = [([UInt8], COpaquePointer)]()
+  var store = [([UInt8], OpaquePointer?)]()
   store.reserveCapacity(128)
   PATH: while store.count >= 0 { // Trick the compiler.
     let cap = UnsafePointer<CChar>(pb)
@@ -135,14 +140,14 @@ func peekDir(pathBytes: [UInt8]) {
       }
     }
     DIRP: while store.count >= 0 { // Trick the compiler.
-      var entry = readdir(dp)
+      var entry = readdir(dp!)
       while entry != nil {
         count += 1
-        if entry.memory.d_type == 4 {
-          var dirName = entry.memory.d_name
+        if entry!.pointee.d_type == 4 {
+          var dirName = entry!.pointee.d_name
           var ignore = true
           withUnsafePointer(&dirName) { ptr in
-            let len = Int(Sys.strlen(UnsafePointer<CChar>(ptr)))
+            let len = Int(Sys.strlen(sp: UnsafePointer<CChar>(ptr)))
             let b = UnsafeBufferPointer<UInt8>(start: UnsafePointer<UInt8>(ptr),
                 count: len)
             if len <= 2 && b[0] == 46 && (len == 1 || b[1] == 46) {
@@ -155,7 +160,7 @@ func peekDir(pathBytes: [UInt8]) {
               cp += b
               cp.append(47)
               cp.append(0) // Now append the trailing null-byte 0
-              store.append((pb, dp))
+              store.append((pb, dp!))
               pb = cp
               ignore = false
             }
@@ -164,9 +169,9 @@ func peekDir(pathBytes: [UInt8]) {
             continue PATH
           }
         }
-        entry = readdir(dp)
+        entry = readdir(dp!)
       }
-      closedir(dp)
+      closedir(dp!)
       if store.count > 0 {
         (pb, dp) = store.removeLast()
         continue DIRP
@@ -180,41 +185,34 @@ func peekDir(pathBytes: [UInt8]) {
 
 
 var sw = Stopwatch()
-var a = [UInt8]("/".utf8)
+var samplePath = "/"
+var a = [UInt8](samplePath.utf8)
 a.append(0)
 sw.start()
-trackDir(a)
+trackDir(pathBytes: a)
 p("trackDir   count: \(count) Elapsed: \(sw.millis)")
 
 count = 0
 sw.start()
-spelunkDir("/")
+spelunkDir(path: samplePath)
 p("spelunkDir count: \(count) Elapsed: \(sw.millis)")
 
 count = 0
 sw.start()
-do {
-  try browseDir(a)
-} catch {
-  // ignore errors.
-}
+try browseDir(pathBytes: a)
 p("browseDir  count: \(count) Elapsed: \(sw.millis)")
 
 count = 0
 sw.start()
-do {
-  try browseDir2(a)
-} catch {
-  // ignore errors.
-}
+try browseDir2(pathBytes: a)
 p("browseDir2 count: \(count) Elapsed: \(sw.millis)")
 
 count = 0
 sw.start()
-peekDir(a)
+peekDir(pathBytes: a)
 p("peekDir    count: \(count) Elapsed: \(sw.millis)")
 
 count = 0
 sw.start()
-trackDir(a)
+trackDir(pathBytes: a)
 p("**trackDir count: \(count) Elapsed: \(sw.millis)")
