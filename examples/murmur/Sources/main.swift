bfc215bbe4c6ae34ebd551d7100057f5b14a647e
Joao Pedrosa <joaopedrosa@gmail.com>
2016-08-04 22:41:30 -0300

    Updated the Murmur example to Swift 3.

--- examples/murmur/Sources/main.swift
+++ examples/murmur/Sources/main.swift
@@ -4,30 +4,30 @@ import Sua
 import CSua
 
 
-p(MurmurHash3.hash32("kinkajou"))
+p(MurmurHash3.hash32(key: "kinkajou"))
 
 var a: [CChar] = [107, 105, 110, 107, 97, 106, 111, 117]
 
-p(MurmurHash3.hash32CChar(a, maxBytes: a.count))
+p(MurmurHash3.hash32CChar(key: a, maxBytes: a.count))
 
 var b: [CChar] = [112, 97, 110, 100, 97]
 
-p(MurmurHash3.hash32CChar(b, maxBytes: b.count, seed: 10))
+p(MurmurHash3.hash32CChar(key: b, maxBytes: b.count, seed: 10))
 
 var c: [UInt8] = [112, 97, 110, 100, 97]
 
-p(MurmurHash3.hash32Bytes(c, maxBytes: c.count, seed: 10))
+p(MurmurHash3.hash32Bytes(key: c, maxBytes: c.count, seed: 10))
 
-p(MurmurHash3.hash32("panda", seed: 10))
+p(MurmurHash3.hash32(key: "panda", seed: 10))
 
 p("-------")
 
-p(MurmurHash3.hash128("kinkajou"))
+p(MurmurHash3.hash128(key: "kinkajou"))
 
-p(MurmurHash3.hash128CChar(a, maxBytes: a.count))
+p(MurmurHash3.hash128CChar(key: a, maxBytes: a.count))
 
-p(MurmurHash3.hash128CChar(b, maxBytes: b.count, seed: 10))
+p(MurmurHash3.hash128CChar(key: b, maxBytes: b.count, seed: 10))
 
-p(MurmurHash3.hash128Bytes(c, maxBytes: c.count, seed: 10))
+p(MurmurHash3.hash128Bytes(key: c, maxBytes: c.count, seed: 10))
 
-p(MurmurHash3.hash128("panda", seed: 10))
+p(MurmurHash3.hash128(key: "panda", seed: 10))
